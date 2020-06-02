b3737ad85ce2f61f1aa437b16e35da7493b410b1
Ante Pettersson <ante.pettersson@gmail.com>
2020-06-02 03:00:00 -0400

    plot1.R: plotting works and data loading is smart

--- plot1.R
+++ plot1.R
@@ -1,21 +1,30 @@
 #read data, it's quite big so try to speed it up by predetermining column classes
 
 #first, find out column names and classes
-myData <- read.table("household_power_consumption.txt", nrows=5, header=TRUE, sep=';', na.strings='?')
+myData <- read.table("household_power_consumption.txt", nrows=5, header=TRUE, 
+                     sep=';', na.strings='?')
 cclasses <- vapply(myData, class, "character")
 cnames <- colnames(myData)
 
 #then, find out which rows are in the timeframe we want
 just_date <- c("character", rep("NULL",8))
-myData <- read.table("household_power_consumption.txt", header=TRUE, sep=';', na.strings='?', colClasses = just_date)
+myData <- read.table("household_power_consumption.txt", header=TRUE, sep=';', 
+                     na.strings='?', colClasses = just_date)
 myData$converted_date <- as.Date(myData$Date, format="%d/%m/%Y")
 startdate <- as.Date("2007-02-01")
 stopdate <- as.Date("2007-02-02")
 selected <- myData[(myData$converted_date >= startdate) & (myData$converted_date <=stopdate),]
 
-myData <- read.table("household_power_consumption.txt", nrows=2880, skip=66637, sep=';', colClasses=cclasses, col.names=cnames)
-#myData$Date <- as.Date(myData$Date, format="%d/%m/%Y")
-#myData$Time <- strptime(myData$Time, format="%H:%M:%S")
-#myData$timestamp <- paste(myData$Date, myData$Time, sep=",")
-myData$timestamp <- strptime(paste(myData$Date, myData$Time, sep=","), format="%d/%m/%Y,%H:%M:%S")
-# 2007-02-01 and 2007-02-02. (february)
+startrow<-as.numeric(rownames(head(selected, 1)))
+stoprow <- as.numeric(rownames(tail(selected, 1)))
+
+#read actual data
+# we don't use header=TRUE this time so we can skip startrows.
+myData <- read.table("household_power_consumption.txt", nrows=(stoprow-startrow+1),
+                     skip=startrow, sep=';', colClasses=cclasses, col.names=cnames)
+myData$timestamp <- strptime(paste(myData$Date, myData$Time, sep=","), 
+                             format="%d/%m/%Y,%H:%M:%S")
+
+#plot 1
+hist(myData$Global_active_power, col="red",
+     main="Global Active Power", xlab ="Global Active Power (kilowatts)")
